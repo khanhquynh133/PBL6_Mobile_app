318379be342385b7057ca0576e7dc43c48198e4e
vanchinhduong2307 <vanchinhduong2307@gmail.com>
2021-12-11 14:08:10 +0700

    feat: create component card job,card job de and page total job posting

---
+++ src/consts/plants.js
@@ -0,0 +1,63 @@
+/** @format */
+
+const plants = [
+	{
+		id: 1,
+		name: "Data Asia House",
+		address: "Nguyen Huu An, DaNang",
+		like: true,
+		img: require("../assets/images/DHA.png"),
+		about:
+			"Succulent Plantis one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+
+	{
+		id: 2,
+		name: "Supreme Tech",
+		address: "Nguyen Huu Tho, Da Nang",
+		like: false,
+		img: require("../assets/images/Supreme.png"),
+		about:
+			"Dragon Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+	{
+		id: 3,
+		name: "Green Global",
+		address: "Tran Phu, Da Nang",
+		like: false,
+		like: false,
+		img: require("../assets/images/TCX.png"),
+		about:
+			"Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+
+	{
+		id: 4,
+		name: "Enlab",
+		address: "Che Viet Tan, Da Nang",
+		like: true,
+		img: require("../assets/images/Enlab.png"),
+		about:
+			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+	{
+		id: 5,
+		name: "KMS",
+		address: "2/9 Street, Da Nang",
+		like: true,
+		img: require("../assets/images/KMS.png"),
+		about:
+			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+	{
+		id: 6,
+		name: "FPT",
+		address: "Ngu Hanh Son, Da Nang",
+		like: false,
+		img: require("../assets/images/FPT.png"),
+		about:
+			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+];
+
+export default plants;
---
+++ src/screens/applicant/TotalJobPosting.js
@@ -0,0 +1,126 @@
+/** @format */
+
+import React from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  Dimensions,
+  TextInput,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import COLORS from "../../consts/colors";
+import jobs from "../../consts/jobs";
+import CardJob from "../../components/Applicant/CardJob";
+const width = Dimensions.get("window").width / 2 - 30;
+
+const TotalJobPosting = ({ navigation }) => {
+  return (
+    <SafeAreaView
+      style={{ flex: 1, paddingHorizontal: 20, backgroundColor: COLORS.white }}
+    >
+      <View style={style.header}>
+        <View>
+          <Text
+            style={{
+              fontSize: 38,
+              color: COLORS.blue,
+              fontWeight: "bold",
+              marginLeft: 30,
+            }}
+          >
+            HREO
+          </Text>
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Job Postings
+          </Text>
+        </View>
+      </View>
+
+      <View style={{ marginTop: 30, marginBottom: 20, flexDirection: "row" }}>
+        <View style={style.searchContainer}>
+          <Icon name="search" size={25} style={{ marginLeft: 20 }} />
+          <TextInput placeholder="Search" style={style.input} />
+        </View>
+      </View>
+      <FlatList
+        columnWrapperStyle={{ justifyContent: "center" }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 10,
+          marginLeft: 20,
+          paddingBottom: 50,
+        }}
+        numColumns={2}
+        data={jobs}
+        renderItem={({ item }) => {
+          return <CardJob job={item} />;
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  categoryContainer: {
+    flexDirection: "row",
+    marginTop: 30,
+    marginBottom: 20,
+    justifyContent: "space-between",
+  },
+  categoryText: { fontSize: 16, color: "grey", fontWeight: "bold" },
+  categoryTextSelected: {
+    color: COLORS.blue,
+    paddingBottom: 5,
+    borderBottomWidth: 2,
+    borderColor: COLORS.blue,
+  },
+  card: {
+    justifyContent: "center",
+    height: 225,
+    backgroundColor: COLORS.light,
+    width,
+    marginHorizontal: 2,
+    borderRadius: 10,
+    marginBottom: 20,
+    padding: 15,
+  },
+  header: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  searchContainer: {
+    height: 50,
+    backgroundColor: COLORS.light,
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    fontSize: 18,
+    fontWeight: "bold",
+    flex: 1,
+    color: COLORS.dark,
+  },
+  sortBtn: {
+    marginLeft: 10,
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    backgroundColor: COLORS.blue,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default TotalJobPosting;
