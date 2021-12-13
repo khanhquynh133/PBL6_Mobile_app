6d46cac50c7f78e2e0b435ee296754e8cf0ff767
khanhquynh133 <65670847+khanhquynh133@users.noreply.github.com>
2021-12-13 11:52:42 +0700

    edit UI of screen POST in screen HOMEPAGEAPPLICANT

--- src/components/Applicant/CardJob.js
+++ src/components/Applicant/CardJob.js
@@ -1,59 +1,68 @@
+/** @format */
+
 import * as React from "react";
-import { Text, View, Image, TouchableOpacity } from "react-native";
-import styles from "./style";
+import { Text, View, Image, TouchableOpacity, Dimensions } from "react-native";
+import COLORS from "../../consts/colors";
+import Icon from "react-native-vector-icons/MaterialIcons";
+const width = Dimensions.get("window").width / 2 - 30;
 const CardJob = ({ job }) => {
-  return (
-    <TouchableOpacity activeOpacity={0.8} onPress={() => {}}>
-      <View style={styles.cartJob}>
-        <View
-          style={{
-            height: 80,
-          }}
-        >
-          <Image
-            source={job.img}
-            style={{
-              width: 80,
-              height: 80,
-              resizeMode: "center",
-              marginLeft: 25,
-              flex: 1,
-            }}
-            r
-          />
-        </View>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 17,
-            marginTop: 10,
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          {job.name}
-        </Text>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginTop: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 12,
-              fontWeight: "bold",
-              textAlign: "center",
-              justifyContent: "center",
-            }}
-          >
-            {job.description}
-          </Text>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity activeOpacity={0.8} onPress={() => {}}>
+			<View
+				style={{
+					justifyContent: "center",
+					height: 225,
+					backgroundColor: COLORS.light,
+					width,
+					marginHorizontal: 2,
+					borderRadius: 10,
+					marginBottom: 20,
+					padding: 15,
+				}}>
+				<View
+					style={{
+						height: 80,
+					}}>
+					<Image
+						source={job.img}
+						style={{
+							alignItems: "center",
+							width: 100,
+							height: 100,
+						}}
+						r
+					/>
+				</View>
+				<Text
+					style={{
+						fontWeight: "bold",
+						fontSize: 17,
+						marginTop: 40,
+						alignItems: "center",
+					}}>
+					{job.name}
+				</Text>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						marginTop: 5,
+					}}>
+					<Text
+						style={{
+							flexDirection: "row",
+							justifyContent: "space-between",
+							marginTop: 5,
+						}}>
+						{job.description}
+					</Text>
+					<TouchableOpacity onPress={() => navigation.navigate("Company")}>
+						<Icon name='favorite-border' size={20} />
+					</TouchableOpacity>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 export default CardJob;
