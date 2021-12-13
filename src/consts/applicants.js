f7d65fb137ed687a791e487df2a548eae4131db9
khanhquynh133 <65670847+khanhquynh133@users.noreply.github.com>
2021-12-13 13:50:30 +0700

    Page of Applicant

---
+++ src/consts/applicants.js
@@ -0,0 +1,70 @@
+/** @format */
+
+const applicants = [
+	{
+		id: 1,
+		name: "Hoang Quynh",
+		address: "Data House Asia",
+		like: true,
+		img: require("../assets/images/HR/quynh.jpg"),
+		description: "ReactJS Intern",
+		about:
+			"Succulent Plantis one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+
+	{
+		id: 2,
+		name: "Trang Huynh",
+		address: "Green Global",
+		like: false,
+		img: require("../assets/images/HR/trang.png"),
+		description: "ReactJS Intern",
+		about:
+			"Dragon Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+	{
+		id: 3,
+		name: "Huyen Nguyen",
+		address: "Green Global",
+		like: false,
+		like: false,
+		img: require("../assets/images/HR/huyen.png"),
+		description: "ReactJS Intern",
+		about:
+			"Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+
+	{
+		id: 4,
+		name: "Nguyen Dang",
+		address: "One Tech Solution",
+		like: true,
+		img: require("../assets/images/HR/nguyen.jpg"),
+		description: "ReactJS Intern",
+		about:
+			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+	{
+		id: 5,
+		name: "Chinh Duong",
+		address: "One Tech Solution",
+		like: true,
+		img: require("../assets/images/HR/chinh.jpg"),
+		description: "ReactJS Intern",
+		about:
+			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+	{
+		id: 6,
+		name: "Huy Nguyen",
+		address: "Supreme Tech",
+		like: false,
+		img: require("../assets/images/HR/huy.jpg"),
+		description: "ReactJS Intern",
+		about:
+			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+	},
+];
+
+export default applicants = [
+    ;
---
+++ src/screens/applicant/HomePageApplicant.js
@@ -0,0 +1,190 @@
+/** @format */
+
+import * as React from "react";
+import { Text, View, Image } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import HomeApplicant from "../applicant/HomeApplicant";
+import TotalJobPosting from "./TotalJobPosting";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+const Stack = createStackNavigator();
+function HomePage({ navigation }) {
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+			initialRouteName='Home'>
+			<Stack.Screen name='Home' component={HomeApplicant} />
+		</Stack.Navigator>
+	);
+}
+
+function Post() {
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+			initialRouteName='JobPosts'>
+			<Stack.Screen name='JobPosts' component={TotalJobPosting} />
+		</Stack.Navigator>
+		// <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+		// 	<Text>Settings!</Text>
+		// </View>
+	);
+}
+
+function Fav() {
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+			initialRouteName='JobPosts'>
+			<Stack.Screen name='JobPosts' component={TotalJobPosting} />
+		</Stack.Navigator>
+	);
+}
+
+function Setting({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<TouchableOpacity
+				icon='home'
+				style={{
+					alignItems: "center",
+					justifyContent: "center",
+					backgroundColor: "#426EB4",
+					padding: 10,
+					height: 35,
+					width: 140,
+					borderRadius: 20,
+					marginLeft: 50,
+					marginRight: 50,
+					marginTop: 20,
+					flexDirection: "row",
+				}}
+				onPress={() => navigation.navigate("Welcome")}>
+				<Icon
+					name='person'
+					size={16}
+					style={{ alignItems: "center", marginRight: 12, color: "#FFF" }}
+				/>
+				<Text
+					style={{
+						fontWeight: "bold",
+						fontSize: 13,
+						color: "#FFF",
+						textAlign: "center",
+					}}>
+					EDIT INFOR
+				</Text>
+			</TouchableOpacity>
+			<TouchableOpacity
+				style={{
+					alignItems: "center",
+					justifyContent: "center",
+					backgroundColor: "#426EB4",
+					padding: 10,
+					height: 35,
+					width: 140,
+					borderRadius: 20,
+					marginLeft: 50,
+					marginRight: 50,
+					marginTop: 20,
+					flexDirection: "row",
+				}}
+				onPress={() => navigation.navigate("Welcome")}>
+				<Icon
+					name='logout'
+					size={16}
+					style={{ alignItems: "center", marginRight: 12, color: "#FFF" }}
+				/>
+				<Text
+					style={{
+						fontWeight: "bold",
+						fontSize: 13,
+						color: "#FFF",
+						textAlign: "center",
+					}}>
+					LOGOUT
+				</Text>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+	return (
+		<Tab.Navigator
+			initialRouteName='HomePage'
+			screenOptions={{
+				tabBarActiveTintColor: "#e91e63",
+				tabBarVisible: false,
+			}}>
+			<Tab.Screen
+				name='HomePage'
+				component={HomePage}
+				options={{
+					tabBarLabel: "Home",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name='home' color={color} size={size} />
+					),
+				}}
+			/>
+
+			<Tab.Screen
+				name='New Feed'
+				component={Post}
+				options={{
+					tabBarLabel: "New Feed",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons
+							name='newspaper-variant-outline'
+							color={color}
+							size={size}
+						/>
+					),
+				}}
+			/>
+
+			<Tab.Screen
+				name='Favorite Post'
+				component={Fav}
+				options={{
+					tabBarLabel: "Favorite Post",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name='heart' color={color} size={size} />
+					),
+				}}
+			/>
+
+			<Tab.Screen
+				name='Setting'
+				component={Setting}
+				options={{
+					tabBarLabel: "Setting",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons
+							name='settings-helper'
+							color={color}
+							size={size}
+						/>
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	);
+}
+
+const HomePageApplicant = () => {
+	return <MyTabs />;
+};
+export default HomePageApplicant;
