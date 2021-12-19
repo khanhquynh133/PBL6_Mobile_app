2f67a131a65903d20ae0b36775ca9ac5d10b0eb9
khanhquynh133 <65670847+khanhquynh133@users.noreply.github.com>
2021-12-19 08:25:21 +0700

    seed data and pass parameters to routes

--- src/consts/companies.js
+++ src/consts/companies.js
@@ -8,8 +8,11 @@ const companies = [
 		like: true,
 		img: require("../assets/images/DHA.png"),
 		description: "ReactJS Intern",
+		rate: "4.9",
+		contact:
+			"Mail: aloha@datahouse.com \nAddress: Da Nang Office: Lot 45, Nguyen Huu An street, Son Tra District, Danang city, Vietnam",
 		about:
-			"Succulent Plantis one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+			"We are 200+ professional software engineers with 10 years of experience in delivering superior products.",
 	},
 
 	{
@@ -19,8 +22,11 @@ const companies = [
 		like: false,
 		img: require("../assets/images/Supreme.png"),
 		description: "ReactJS Intern",
+		rate: "4.8",
+		contact:
+			"Mail: info@supremetech.vn \nAddress: 2F Ricco building, 363 Nguyen Huu Tho, Cam Le Dist., Da Nang, Viet Nam",
 		about:
-			"Dragon Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+			"We seek for joy in every corner of our work because success follows those who enjoys working.",
 	},
 	{
 		id: 3,
@@ -30,8 +36,11 @@ const companies = [
 		like: false,
 		img: require("../assets/images/TCX.png"),
 		description: "ReactJS Intern",
+		rate: "4.8",
+		contact:
+			"Mail: contact@greenglobal.vn \nAddress: 31 Trần Phú, Quận Hải Châu, Thành phố Đà Nẵng, Việt Nam",
 		about:
-			"Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+			"Với kỹ năng phân tích nghiệp vụ đa dạng, chúng tôi đã và đang tiến hành xây dựng bộ Sản phẩm và Giải pháp dành cho Chính phủ góp phần hướng đến Chính phủ số tại Việt Nam",
 	},
 
 	{
@@ -41,8 +50,11 @@ const companies = [
 		like: true,
 		img: require("../assets/images/Enlab.png"),
 		description: "ReactJS Intern",
+		rate: "4.7",
+		contact:
+			"Mail: job@enlabsoftware.com \nAddress: 27 Che Viet Tan Street, Da Nang City, Viet Nam",
 		about:
-			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+			"Enlab Software is a young and enthusiastic tech company based in Danang, Vietnam. At our core, we provide Agile Offshore Software Development for enterprises, startups, IT consulting vendors, digital agencies across the sea.",
 	},
 	{
 		id: 5,
@@ -51,18 +63,37 @@ const companies = [
 		like: true,
 		img: require("../assets/images/KMS.png"),
 		description: "ReactJS Intern",
+		rate: "4.8",
+		contact:
+			"Address: 346, 2/9 Street, Hai Chau District, Da Nang City, Vietnam",
 		about:
-			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+			"KMS Technology is a leading provider of offshore software development, testing services, and consulting. Our engineers bring organizations faster speed to market so they can stay focused on growing their core business.",
 	},
 	{
 		id: 6,
+		name: "One Tech Solution",
+		address: "2/9 Street, Da Nang",
+		like: true,
+		img: require("../assets/images/FPT.png"),
+		description: "ReactJS Intern",
+		rate: "4.8",
+		contact:
+			"Mail: ots.info@one-line.com \nAddress: Floor 3 -4 Phi Long Technology Building, 52 Nguyen Van Linh, Da Nang, Viet Nam",
+		about:
+			"We provide IT solutions and support to all customers and partners of Ocean Network Express (ONE).",
+	},
+	{
+		id: 7,
 		name: "FPT",
 		address: "Ngu Hanh Son, Da Nang",
 		like: false,
 		img: require("../assets/images/FPT.png"),
 		description: "ReactJS Intern",
+		rate: "4.8",
+		contact:
+			"Mail: fsodn.contact@fsoft.com.vn \nAddress: FPT Complex Building, Nam Ky Khoi Nghia Street, Hoa Hai Ward, Ngu Hanh Son District, Danang City, Vietnam",
 		about:
-			"Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.",
+			"FPT Software is the global leading technology and IT services group headquartered in Vietnam with nearly US$2 billion in revenue and 32000 employees.",
 	},
 ];
 
