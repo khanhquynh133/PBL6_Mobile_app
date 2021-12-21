aac2149b9aaf52071f8cd0da1fb2c35b74c32cb7
vanchinhduong2307 <vanchinhduong2307@gmail.com>
2021-12-22 01:41:41 +0700

    feat: fetch Api login

--- src/screens/general/Login.js
+++ src/screens/general/Login.js
@@ -2,195 +2,185 @@
 
 import React, { useState } from "react";
 import {
-	StyleSheet,
-	Text,
-	View,
-	Button,
-	Image,
-	TextInput,
-	TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
+import HomePageApplicant from "../applicant/HomeApplicant";
 import rest from "../../api/rest";
 import { USER_INFORMATION_ENTRY_POINT, GET } from "../../utils/apiRequest";
-import * as Notifications from "expo-notifications";
-
+import Icon from "react-native-vector-icons/FontAwesome5";
+import jwt_decode from "jwt-decode";
+import { GET_TOKEN_URL } from "../../config";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const Login = ({ navigation }) => {
-	const [dataLogin, setDataLogin] = useState({
-		username: "",
-		password: "",
-	});
+  const [dataLogin, setDataLogin] = useState({
+    username: "",
+    password: "",
+  });
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isWrongPassword, setWrongPassword] = useState(false);
+  const handleLogin = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: "Basic SHJlb19BcHA6MXEydzNFKg==",
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      withCredentials: true,
+      data: new URLSearchParams({
+        username: dataLogin.username,
+        password: dataLogin.password,
+        grant_type: "password",
+      }),
+    };
+    try {
+      const response = await axios(GET_TOKEN_URL, options);
+      const access_token = response.data.access_token;
+      await AsyncStorage.setItem("_token", access_token);
+      const { role } = jwt_decode(access_token);
+      if (role === "hr") {
+        // navigate  admin page
+        navigation.navigate("HPAdmin");
+      } else {
+        // navigate page applicant
+        navigation.navigate("HPApplicant");
+      }
+    } catch (error) {
+      setWrongPassword(!isWrongPassword);
+    }
+  };
 
-	const handleChange = async () => {
-		const data = {
-			username: dataLogin.username, //'admin',
-			password: dataLogin.password, //'1q2w3E*',
-			scope: "Hreo",
-			grant_type: "password",
-		};
-		//console.log("data :>> ", data);
-		var formBody = [];
-		for (var property in data) {
-			var encodedKey = encodeURIComponent(property);
-			var encodedValue = encodeURIComponent(data[property]);
-			formBody.push(encodedKey + "=" + encodedValue);
-		}
-		formBody = formBody.join("&");
-
-		dataLogin.grant_type = "password";
-		let s = new URLSearchParams(Object.entries(dataLogin)).toString();
-		//console.log(dataLogin, s);
-		try {
-			const { status } = await Notifications.getPermissionsAsync();
-			if (status != "granted") {
-				const { status } = await Notifications.requestPermissionsAsync();
-
-				if (status != "granted") {
-					alert("Lấy token thất bại");
-					return;
-				}
-			}
-			// token = (await Notifications.getExpoPushTokenAsync()).data;
-			// console.log(token);
-			await rest
-				.login(formBody)
-				.then((res) => {
-					console.log(res);
-					rest
-						.request(GET, USER_INFORMATION_ENTRY_POINT)
-						.then((res) => {
-							console.log(res);
-							const role = res?.userAbp?.roles?.find((role) => role.name);
-							console.log(role?.name);
-							if (role?.name === "admin") {
-								navigation.navigate("HPAdmin");
-							} else if (role?.name === "hr") {
-								navigation.navigate("HPHR");
-							} else {
-								navigation.navigate("HPApplicant");
-							}
-						})
-						.catch((err) => console.log(err));
-
-					// navigation.navigate("HPA");
-				})
-				.catch((err) => console.log(err));
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	// const handleLogin = async () => {
-	// 	const rs = await rest.login(dataLogin);
-	// 	console.log(rs);
-	// };
-
-	return (
-		<View style={styles.container}>
-			<Image
-				style={styles.logo}
-				source={require("../../assets/images/logo.png")}
-			/>
-
-			<Text style={styles.login}>LOGIN</Text>
-			<TextInput
-				name='username'
-				style={styles.input}
-				// onChangeText={handleChange}
-				onChangeText={(text) =>
-					setDataLogin({
-						...dataLogin,
-						username: text,
-					})
-				}
-				value={dataLogin.username}
-				placeholder='Email or Username'
-			/>
-			<TextInput
-				name='password'
-				secureTextEntry={true}
-				style={styles.input}
-				// onChangeText={handleChange}
-				onChangeText={(text) =>
-					setDataLogin({
-						...dataLogin,
-						password: text,
-					})
-				}
-				value={dataLogin.password}
-				placeholder='password'
-			/>
-
-			{/* <TouchableOpacity style={styles.button}>
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../../assets/images/logo.png")}
+      />
+      <Text style={styles.login}>LOGIN</Text>
+      <TextInput
+        name="username"
+        style={styles.input}
+        // onChangeText={handleChange}
+        onChangeText={(text) =>
+          setDataLogin({
+            ...dataLogin,
+            username: text,
+          })
+        }
+        value={dataLogin.username}
+        placeholder="Email or Username"
+      />
+      <TextInput
+        name="password"
+        secureTextEntry={isShowPassword ? false : true}
+        style={styles.input}
+        // onChangeText={handleChange}
+        onChangeText={(text) =>
+          setDataLogin({
+            ...dataLogin,
+            password: text,
+          })
+        }
+        value={dataLogin.password}
+        placeholder="password"
+      />
+      {/* can you css at here so that icon can horizontal with the Text Password */}
+      <TouchableOpacity>
+        <Icon
+          name={isShowPassword ? "eye" : "eye-slash"}
+          onPress={() => {
+            setIsShowPassword(!isShowPassword);
+          }}
+          size={20}
+        />
+      </TouchableOpacity>
+      {/* <TouchableOpacity style={styles.button}>
 				<Button
 					onPress={handleChange}
 					onPress={() => navigation.navigate("HPAdmin")}
 					style={styles.textBtn}
 					title='LOGIN'></Button>
 			</TouchableOpacity> */}
-			<TouchableOpacity
-				style={styles.button}
-				onPress={() => navigation.navigate("HPAdmin")}>
-				<Text style={styles.textBtn}>CONTINUE</Text>
-			</TouchableOpacity>
-
-			<TouchableOpacity onPress={() => navigation.navigate("Register")}>
-				<Text style={styles.baseText}>
-					Dont have account? Create an account
-				</Text>
-			</TouchableOpacity>
-		</View>
-	);
+      {isWrongPassword && (
+        <Text style={styles.warningText}>Wrong Username or Password</Text>
+      )}
+      <TouchableOpacity
+        style={styles.button}
+        // onPress={() => navigation.navigate("HPAdmin")}
+        onPress={() => handleLogin()}
+      >
+        <Text style={styles.textBtn}>CONTINUE</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+        <Text style={styles.baseText}>
+          Dont have account? Create an account
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 export default Login;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#E0FFFF",
-		alignItems: "center", //giữa trang
-		justifyContent: "center",
-	},
-	logo: {
-		width: 60,
-		height: 60,
-		marginBottom: 20,
-	},
-	login: {
-		fontWeight: "bold",
-		fontSize: 30,
-		color: "#426EB4",
-		marginBottom: 25,
-	},
-	baseText: {
-		fontWeight: "bold",
-		fontSize: 10,
-		color: "#426EB4",
-		marginTop: 10,
-	},
-	input: {
-		height: 40,
-		margin: 12,
-		borderBottomWidth: 1,
-		padding: 10,
-	},
-	button: {
-		alignItems: "center",
-		backgroundColor: "#426EB4",
-		padding: 10,
-		height: 35,
-		width: 130,
-		borderRadius: 20,
-		marginLeft: 50,
-		marginRight: 50,
-		marginTop: 30,
-		marginBottom: 15,
-	},
-	textBtn: {
-		fontWeight: "bold",
-		fontSize: 16,
-		color: "#FFF",
-		textAlign: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#E0FFFF",
+    alignItems: "center", //giữa trang
+    justifyContent: "center",
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    marginBottom: 20,
+  },
+  login: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "#426EB4",
+    marginBottom: 25,
+  },
+  baseText: {
+    fontWeight: "bold",
+    fontSize: 10,
+    color: "#426EB4",
+    marginTop: 10,
+  },
+  warningText: {
+    fontWeight: "bold",
+    fontSize: 10,
+    color: "#FF0000",
+    marginTop: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#426EB4",
+    padding: 10,
+    height: 35,
+    width: 130,
+    borderRadius: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    marginTop: 30,
+    marginBottom: 15,
+  },
+  textBtn: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#FFF",
+    textAlign: "center",
+  },
 });
 
 // /** @format */
